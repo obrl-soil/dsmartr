98a858ac7b1febc69d927f305cae2952542a0e18
obrl_soil <leobrien@gmail.com>
2017-08-20 17:21:40 +1000

    improve polygon checker
    params passed properly (also new check for non-unique IDs)

--- R/dsmartr_helpers.R
+++ R/dsmartr_helpers.R
@@ -25,19 +25,22 @@ n_things <- function(input = NULL, selector = NULL) {
 #' Checks dsmartr input polygons for potential data quality issues.
 #' @param src_map An sfc_POLYGON/MULTIPOLYGON or SpatialPolygonsDataFrame object.
 #' @param id_field String; name of unique identifier field.
+#' @param cs String; stub of attribute column names holding soil class data
+#' @param ps String; stub of attribute column names folding soil percentage data
 #' @details This function highlights some issues that may cause dsmartr to either fail or behave
 #'   unpredictably. Usually these are side-effects of prepration work carried out in other software.
 #'   Sometimes they are the result of errors in the source data. Note that polygon geometry is not
-#'   explicitly checked, only attributes. At present, three problems are checked - whether data is
-#'   missing (e.g. a polygon class without a matching percentage), whether a percentage is 0%, and
-#'   whether total percentage (by polygon) is not 100%.
+#'   explicitly checked, only attributes. At present, four problems are checked - whether data is
+#'   missing (e.g. a polygon class without a matching percentage), whether a percentage is 0%,
+#'   whether total percentage (by polygon) is not 100%, and whether any IDs are duplicated.
 #' @return An sf object with appended logical columns indicating presence of possible data faults.
 #' @examples \dontrun{
 #' load("heronvale_soilmap")
 #' checked_map <- dsmartr_check_polygons(src_map = heronvale_soilmap,
 #' id_field = 'POLY_NO')}
 #' @export
-dsmartr_check_polygons <- function(src_map  = NULL, id_field = NULL) {
+dsmartr_check_polygons <- function(src_map  = NULL, id_field = NULL,
+                                   cs = NULL, ps = NULL) {
 
   # coerce to sf
   src_map <- if(is(src_map, 'sf') == FALSE) {
@@ -50,24 +53,25 @@ dsmartr_check_polygons <- function(src_map  = NULL, id_field = NULL) {
   src_split <- split(src_map, 1:nrow(src_map))
 
   missing_data <- purrr::map_lgl(src_split, function(md) {
-               n_classes <- length(n_things(md, 'CLASS_'))
-               n_percs   <- length(n_things(md, 'PERC_'))
+               n_classes <- length(n_things(md, cs))
+               n_percs   <- length(n_things(md, ps))
                ifelse(n_classes != n_percs, TRUE, FALSE)
                })
 
   zero_percs <- purrr::map_lgl(src_split, function(zp) {
-               poly_percs <- n_things(zp, 'PERC_')
+               poly_percs <- n_things(zp, ps)
                ifelse(any(poly_percs == 0), TRUE, FALSE)
              })
 
   problem_percs <- purrr::map_lgl(src_split, function(sps) {
-               total <- sum(n_things(sps, 'PERC_'))
+               total <- sum(n_things(sps, ps))
                ifelse(total != 100, TRUE, FALSE)
                })
 
   src_map$missing_data  <- as.vector(missing_data)
   src_map$zero_percs    <- as.vector(zero_percs)
   src_map$problem_percs <- as.vector(problem_percs)
+  src_map$duplicate_ids <- duplicated(src_map[, id_field])
 
   return(src_map)
 }
